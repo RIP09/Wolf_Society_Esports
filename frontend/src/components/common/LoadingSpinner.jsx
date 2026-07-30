a0bb65592e68f332b4e0ef971ec5e251c0caa593
NxR_Manager <106897988+RIP09@users.noreply.github.com>
2026-07-30 11:06:13 +0530

    Delete frontend directory

--- frontend/src/components/common/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress, Box } from '@mui/material';
-
-export default function LoadingSpinner() {
-  return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', py: 4 }}>
-      <CircularProgress />
-    </Box>
-  );
-}
--- frontend/src/services/auth.js
+++
@@ -1,9 +0,0 @@
-import { api } from './api';
-
-export const login = (email, password) =>
-  api.post('/auth/login', { email, password });
-
-export const register = (userData) =>
-  api.post('/auth/register', userData);
-
-export const getMe = () => api.get('/auth/me');
