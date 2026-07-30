a30599cc6a44432e7988e8df4c72e731f8e5e33c
NxR_Manager <106897988+RIP09@users.noreply.github.com>
2026-07-30 11:05:46 +0530

    Delete backend directory

--- backend/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const logger = require('../config/logger');
-
-module.exports = (err, req, res, next) => {
-  logger.error(err.stack);
-  const status = err.status || 500;
-  res.status(status).json({
-    error: err.message || 'Internal server error',
-  });
-};
